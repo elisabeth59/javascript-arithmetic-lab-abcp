16e1b91b9de5c6ef14fea6e11a8d446b033312f2
Elisabeth Narine <elisabethnarine@gmail.com>
2018-03-01 03:13:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add (a, b) {
+  return a + b
+}
+function subtract (a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b 
+}
+function inc(a) {
+  return a + 1
+}
+function dec(a) {
+  return a - 1
+}
+function makeInt(string) {
+  return string.a
+}
+function makeInt('02328') { 
+ return (0, 10)
+}
